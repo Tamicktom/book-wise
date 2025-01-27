9e29b642bfa3b88653f02c51092c13a235e73fd0
Tamicktom <rickpokemon38@gmail.com>
2025-01-27 03:16:13 -0300

    refactor: extract book card rendering into separate function

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ $search = $_GET['search'] ?? '';
 
 // import books from books.php
 require_once '_books.php';
+require_once 'book-card.php';
 
 $filtered_books = [];
 
@@ -66,39 +67,11 @@ if ($search) {
     <section class="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3">
 
       <?php
+      /**
+       * Loop through the filtered books and display them
+       */
       foreach ($filtered_books as $book) {
-        // display the books
-        $rating = '';
-        for ($i = 0; $i < $book['rating']; $i++) {
-          $rating .= '⭐';
-        }
-
-        $book_url = "/book.php?id=" . $book['id'];
-
-        echo '
-            <div class="grid h-48 grid-cols-5 col-span-1 gap-2 p-2 transition-all border-2 rounded border-stone-800 hover:bg-stone-800">
-            <a href=' . $book_url . ' class="h-full col-span-2 overflow-hidden rounded">
-              <img
-                src="' . $book['image'] . '"
-                alt="Imagem do livro"
-                class="object-cover size-full" />
-            </a>
-            <div class="col-span-3 flex flex-col justify-start gap-2">
-              <a href=' . $book_url . ' class="font-semibold hover:underline">
-                ' . $book['title'] . '
-              </a>
-              <div class="text-xs italic">
-                ' . $book['author'] . '
-              </div>
-              <div class="text-xs italic">
-                ' . $rating . '
-              </div>
-              <div class="text-sm text-stone-400 h-fit line-clamp-3">
-                ' . $book['description'] . '
-              </div>
-            </div>
-          </div>
-          ';
+        book_card($book);
       }
       ?>
 
