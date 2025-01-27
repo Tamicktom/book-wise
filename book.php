de451f897c70f0019ec397fc1313ba79f10c0f88
Tamicktom <rickpokemon38@gmail.com>
2025-01-27 03:39:23 -0300

    refactor: update book title access to use object property

--- book.php
+++ book.php
@@ -16,12 +16,12 @@ $book = $books[$book_id - 1];
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-  <title>Bookwise - <?= $book['title'] ?></title>
+  <title>Bookwise - <?= $book->title ?></title>
 </head>
 
 <body>
   <h1>
-    <?= $book['title'] ?>
+    <?= $book->title ?>
   </h1>
 </body>
 
