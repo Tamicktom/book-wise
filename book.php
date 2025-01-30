77f6ccac7211d22ea61f838f8ea448a56cc7b453
Tamicktom <rickpokemon38@gmail.com>
2025-01-30 02:13:22 -0300

    refactor: simplify book retrieval logic using arrow function

--- book.php
+++ book.php
@@ -5,9 +5,7 @@ $book_id = $_GET['id'];
 //import books from books.php
 require_once '_books.php';
 
-$book = array_filter($books, function ($book) use ($book_id) {
-  return $book->id == $book_id;
-});
+$book = array_filter($books, fn($book) => $book->id == $book_id);
 
 $book = array_pop($book);
 
