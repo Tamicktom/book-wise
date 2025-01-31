66aedd89f98d4425b6c6af4e86395ea58fa2e11a
Tamicktom <rickpokemon38@gmail.com>
2025-01-30 23:14:10 -0300

    add routing

--- index.php
+++ index.php
@@ -2,6 +2,24 @@
 
 require "_books.php";
 
-$view = "index";
+$request_url = $_SERVER["REQUEST_URI"];
+
+$routes = [
+  "/" => "index",
+  "/book" => "book",
+];
+
+function clearRouteSearchParams($route)
+{
+  return explode("?", $route)[0];
+}
+
+function getRouteOrDefault($routes, $request_url)
+{
+  $route = clearRouteSearchParams($request_url);
+  return $routes[$route] ?? "404";
+}
+
+$view = getRouteOrDefault($routes, $request_url);
 
 require "views/template/app.php";
