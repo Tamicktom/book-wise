d5d69d55a995139f6ce52a7408a982e8e0134ca7
Tamicktom <rickpokemon38@gmail.com>
2025-01-31 00:27:11 -0300

    ok

--- _books.php
+++ _books.php
@@ -2,42 +2,42 @@
 
 class Book
 {
-  public $id;
-  public $title;
-  public $author;
-  public $rating;
-  public $description;
-  public $image;
+    public string $id;
+    public string $title;
+    public string $author;
+    public string $rating;
+    public string $description;
+    public string $image;
 
-  public function __construct($id, $title, $author, $rating, $description, $image)
-  {
-    $this->id = $id;
-    $this->title = $title;
-    $this->author = $author;
-    $this->rating = $rating;
-    $this->description = $description;
-    $this->image = $image;
-  }
-
-  public function getRating()
-  {
-    $rating = '';
-    for ($i = 0; $i < $this->rating; $i++) {
-      $rating .= '⭐';
+    public function __construct($id, $title, $author, $rating, $description, $image)
+    {
+        $this->id = $id;
+        $this->title = $title;
+        $this->author = $author;
+        $this->rating = $rating;
+        $this->description = $description;
+        $this->image = $image;
     }
-    return $rating;
-  }
 
-  public function getBookUrl()
-  {
-    return "/book.php?id=" . $this->id;
-  }
+    public function getRating()
+    {
+        $rating = '';
+        for ($i = 0; $i < $this->rating; $i++) {
+            $rating .= '⭐';
+        }
+        return $rating;
+    }
 
-  public function render()
-  {
-    $book_url = $this->getBookUrl();
+    public function getBookUrl(): string
+    {
+        return "/book.php?id=" . $this->id;
+    }
 
-    $html = <<<HTML
+    public function render()
+    {
+        $book_url = $this->getBookUrl();
+
+        $html = <<<HTML
       <div class="grid h-48 grid-cols-5 col-span-1 transition-all border-2 rounded border-stone-800 hover:bg-stone-800 overflow-hidden group">
         <a href="{$book_url}" class="h-full col-span-2 overflow-hidden">
           <img
@@ -62,50 +62,52 @@ class Book
       </div>
     HTML;
 
-    echo $html;
-  }
-};
+        echo $html;
+    }
+}
+
+global $books;
 
 $books = array_map(function ($book) {
-  return new Book(
-    $book['id'],
-    $book['title'],
-    $book['author'],
-    $book['rating'],
-    $book['description'],
-    $book['image']
-  );
+    return new Book(
+        $book['id'],
+        $book['title'],
+        $book['author'],
+        $book['rating'],
+        $book['description'],
+        $book['image']
+    );
 }, [
-  [
-    'id' => 1,
-    'title' => 'The Great Gatsby',
-    'author' => 'F. Scott Fitzgerald',
-    'rating' => 4,
-    'description' => 'The Great Gatsby is a novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway\'s interactions with mysterious millionaire Jay Gatsby and Gatsby\'s obsession to reunite with his former lover, Daisy Buchanan.',
-    'image' => 'https://i.pinimg.com/736x/92/9b/6b/929b6bceaea9375dc515c099aad59892.jpg'
-  ],
-  [
-    'id' => 2,
-    'title' => 'The Catcher in the Rye',
-    'author' => 'J. D. Salinger',
-    'rating' => 3,
-    'description' => 'The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951.',
-    'image' => 'https://i.pinimg.com/736x/47/f4/09/47f40920c240110dbb088543612bc6e0.jpg'
-  ],
-  [
-    'id' => 3,
-    'title' => 'The Lord of the Rings',
-    'author' => 'J. R. R. Tolkien',
-    'rating' => 2,
-    'description' => 'The Lord of the Rings is an epic high-fantasy novel by the English author and scholar J. R. R. Tolkien. Set in Middle-earth, the world at some distant time in the past, the story began as a sequel to Tolkien\'s 1937 children\'s book The Hobbit, but eventually developed into a much larger work.',
-    'image' => 'https://i.pinimg.com/736x/ed/dd/df/eddddfb389102ae4dc115c60daf6cc1f.jpg'
-  ],
-  [
-    'id' => 4,
-    'title' => 'Harry Potter and the Philosopher\'s Stone',
-    'author' => 'J. K. Rowling',
-    'rating' => 1,
-    'description' => 'Harry Potter and the Philosopher\'s Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling\'s debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry.',
-    'image' => 'https://i.pinimg.com/736x/a4/16/0d/a4160de9649cff905b7b2c8b4adbc88d.jpg'
-  ]
+    [
+        'id' => 1,
+        'title' => 'The Great Gatsby',
+        'author' => 'F. Scott Fitzgerald',
+        'rating' => 4,
+        'description' => 'The Great Gatsby is a novel by American writer F. Scott Fitzgerald. Set in the Jazz Age on Long Island, near New York City, the novel depicts first-person narrator Nick Carraway\'s interactions with mysterious millionaire Jay Gatsby and Gatsby\'s obsession to reunite with his former lover, Daisy Buchanan.',
+        'image' => 'https://i.pinimg.com/736x/92/9b/6b/929b6bceaea9375dc515c099aad59892.jpg'
+    ],
+    [
+        'id' => 2,
+        'title' => 'The Catcher in the Rye',
+        'author' => 'J. D. Salinger',
+        'rating' => 3,
+        'description' => 'The Catcher in the Rye is a novel by J. D. Salinger, partially published in serial form in 1945–1946 and as a novel in 1951.',
+        'image' => 'https://i.pinimg.com/736x/47/f4/09/47f40920c240110dbb088543612bc6e0.jpg'
+    ],
+    [
+        'id' => 3,
+        'title' => 'The Lord of the Rings',
+        'author' => 'J. R. R. Tolkien',
+        'rating' => 2,
+        'description' => 'The Lord of the Rings is an epic high-fantasy novel by the English author and scholar J. R. R. Tolkien. Set in Middle-earth, the world at some distant time in the past, the story began as a sequel to Tolkien\'s 1937 children\'s book The Hobbit, but eventually developed into a much larger work.',
+        'image' => 'https://i.pinimg.com/736x/ed/dd/df/eddddfb389102ae4dc115c60daf6cc1f.jpg'
+    ],
+    [
+        'id' => 4,
+        'title' => 'Harry Potter and the Philosopher\'s Stone',
+        'author' => 'J. K. Rowling',
+        'rating' => 1,
+        'description' => 'Harry Potter and the Philosopher\'s Stone is a fantasy novel written by British author J. K. Rowling. The first novel in the Harry Potter series and Rowling\'s debut novel, it follows Harry Potter, a young wizard who discovers his magical heritage on his eleventh birthday, when he receives a letter of acceptance to Hogwarts School of Witchcraft and Wizardry.',
+        'image' => 'https://i.pinimg.com/736x/a4/16/0d/a4160de9649cff905b7b2c8b4adbc88d.jpg'
+    ]
 ]);
