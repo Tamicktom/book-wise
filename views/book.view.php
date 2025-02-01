f1fabaaca37992251b6427a074feae0bec42b109
Tamicktom <rickpokemon38@gmail.com>
2025-02-01 12:24:18 -0300

    feat: enhance book view layout with improved HTML structure and styling

--- views/book.view.php
+++ views/book.view.php
@@ -1,4 +1,26 @@
+<main class="grid grid-cols-6 gap-4 p-4">
+  <figure class="col-span-2 flex justify-center items-center">
+    <img
+      src="<?= htmlspecialchars($book->image, ENT_QUOTES, 'UTF-8') ?>"
+      alt="<?= htmlspecialchars($book->title, ENT_QUOTES, 'UTF-8') ?>"
+      class="object-cover w-fit max-h-[512px]"
+    />
+  </figure>
 
-<section>
-  <?= $book->title ?>
-</section>
+  <article class="col-span-4 flex flex-col justify-start gap-2 p-2">
+    <header>
+      <h1 class="font-semibold text-2xl">
+        <?= htmlspecialchars($book->title, ENT_QUOTES, 'UTF-8') ?>
+      </h1>
+      <p class="italic">
+        <?= htmlspecialchars($book->author, ENT_QUOTES, 'UTF-8') ?>
+      </p>
+    </header>
+    <div class="italic">
+      <?= htmlspecialchars($book->getRating(), ENT_QUOTES, 'UTF-8') ?>
+    </div>
+    <p class="text-stone-400 h-fit line-clamp-3">
+      <?= htmlspecialchars($book->description, ENT_QUOTES, 'UTF-8') ?>
+    </p>
+  </article>
+</main>
