3e68814a996224f64d85fe52fe5aa2ae08cd0ef5
Tamicktom <rickpokemon38@gmail.com>
2025-02-15 19:17:21 -0300

    feat: enhance navigation bar by adding button styling for the login link

--- views/template/app.php
+++ views/template/app.php
@@ -10,7 +10,7 @@
 
 <body class="bg-stone-950 text-stone-200">
   <header class="shadow-lg bg-stone-900">
-    <nav class="flex justify-between max-w-screen-lg px-8 py-4 mx-auto">
+    <nav class="flex justify-between max-w-screen-lg px-8 py-4 mx-auto items-center">
       <a href="/">
         <div class="text-xl font-bold tracking-wider">BookWise</div>
       </a>
@@ -24,7 +24,11 @@
       </ul>
       <ul>
         <li>
-          <a href="/login" class="hover:underline">Login</a>
+          <a href="/login" class="hover:underline">
+            <button class="cursor-pointer border border-lime-500 text-lime-500 px-4 py-2 rounded font-bold hover:bg-lime-500 hover:text-stone-900 transition-colors">
+              Login
+            </button>
+          </a>
         </li>
       </ul>
     </nav>
