749289e8dd5d9864901af9d812000ad8f84ac86f
Tamicktom <rickpokemon38@gmail.com>
2025-04-20 11:18:57 -0300

    feat: initialize database schema with users, books, and user_books tables

---
+++ start_db.sql
@@ -0,0 +1,28 @@
+create table
+  users (
+    id integer not null primary key autoincrement,
+    name varchar(255) not null,
+    email varchar(255) not null,
+    password VARCHAR(255) default '' not null
+  );
+
+create table
+  books (
+    id integer primary key,
+    title varchar(255),
+    author varchar(255),
+    description text,
+    release_year integer,
+    rating real,
+    number_of_pages integer,
+    image_url varchar(2047),
+    rating_quantity integer default 0 not null,
+    predominant_color varchar(6) default 'FFFFFF'
+  );
+
+create table
+  user_books (
+    id integer primary key,
+    user_id integer not null references users,
+    book_id integer not null references books
+  );
