8938fe2b4b43857b90ce4e35cf465aa787c1a16f
Tamicktom <rickpokemon38@gmail.com>
2025-07-18 23:09:37 -0300

    chore: clean up whitespace in my-books view file

--- views/my-books.view.php
+++ views/my-books.view.php
@@ -6,13 +6,11 @@
 
 <section class="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3">
   <?php
-
   /**
    * Loop through the filtered books and display them
    */
   foreach ($user_books as $book) {
     $book->render();
   }
-
   ?>
 </section>
